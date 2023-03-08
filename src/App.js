9f6a717dca63b77b7f00854525f2fce81632ad51
Harish-3569 <727721euit049@skcet.ac.in>
2023-03-08 12:47:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Trust from './Trust';
+import Credit from './Credit.js';
+import Debit from './Debit';
+import Post from './Post';
+import Del from './Delete';
+import Get from './Get';
+import Put from './Put';
+//import Debit from './Debit';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Post />
+     <Get />
+     <Put />
+     <Del />
     </div>
   );
 }
